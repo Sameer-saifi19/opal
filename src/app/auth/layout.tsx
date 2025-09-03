f40234fc1bc9bc6385d1d54a5b75e300aa4ecd78
Sameer Saifi <Sameer.edu19@gmail.com>
2025-09-03 10:33:12 +0530

    added clerk auth

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode
+}
+
+const AuthLayout = ({children}: Props) => {
+  return <div className='container h-screen flex justify-center items-center'>{children}</div>
+}
+
+export default AuthLayout
---
+++ src/app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+type Props = {}
+
+const Signin = (props: Props) => {
+  return <SignUp/>
+}
+
+export default Signin
